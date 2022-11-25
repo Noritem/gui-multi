a893f47a0d95ce5bd875b23de8430797ffd9174b
Noritem <71951392+Noritem-Dev@users.noreply.github.com>
2022-11-25 12:26:32 +0100

    Uploaded

---
+++ README.md
@@ -0,0 +1,60 @@
+![License](https://img.shields.io/github/license/litecoinn/gui-multi.svg?style=for-the-badge) ![Repo Size](https://img.shields.io/github/languages/code-size/litecoinn/gui-multi.svg?style=for-the-badge) ![TOP_LANGUAGE](https://img.shields.io/github/languages/top/litecoinn/gui-multi.svg?style=for-the-badge) ![FORKS](https://img.shields.io/github/forks/litecoinn/gui-multi.svg?style=for-the-badge&social) ![Stars](https://img.shields.io/github/stars/litecoinn/gui-multi.svg?style=for-the-badge)
+    
+# Discord GUI Multitool
+
+## Table of Contents
+
+- [Description](#description)
+- [Screenshots](#screenshots)
+- [Built With](#built-with)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Acknowledgements](#acknowledgements)
+- [Contacts](#contacts)
+
+## Description
+
+With This tool U have an GUI Based Multitool For Discord That uses the Discord API
+
+## Screenshots
+
+<img src="https://cdn.discordapp.com/attachments/1040217890089078794/1044337315977375784/unknown.png"/>
+
+<a href="https://www.python.org/"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" height="40px" width="40px" /></a>
+
+## Getting Started
+
+Before u use this here Make Sure:
+
+You know the Basics About Python And The package tkinter And customtkinter and Discord API
+
+### Prerequisites
+
+Python 3.10
+Python knowledge 
+
+### Installation
+
+pip install customtkinter 
+pip install tkinter 
+
+
+## Usage
+
+python main.py
+
+
+## License
+
+<a href="https://choosealicense.com/licenses/apache-2.0/"><img src="https://raw.githubusercontent.com/johnturner4004/readme-generator/master/src/components/assets/images/apache.svg" height=40 />Apache License 2.0</a>
+
+## Acknowledgements
+
+Who helped you make this project a reality? Friends? Family? Contributors? Instructors?
+
+## Contacts
+
+<a href="https://www.linkedin.com/in/"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" /></a>  <a href="mailto:"><img src=https://raw.githubusercontent.com/johnturner4004/readme-generator/master/src/components/assets/images/email_me_button_icon_151852.svg /></a>
---
+++ main.py
@@ -0,0 +1,110 @@
+import customtkinter
+import tkinter
+from time import sleep
+import requests
+from time import sleep
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("dark-blue")
+     
+class tokeninfo():
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.title("Token Info")
+        self.root.geometry("300x200")
+        self.root.resizable(False, False)
+        self.root.iconbitmap("icon.ico")
+        self.root.configure(bg="#856ff8")
+        #make ip text box
+        self.token = customtkinter.CTkEntry(self.root)
+        self.token.pack()
+        #make button
+        self.button = customtkinter.CTkButton(self.root, text="Enter", command=self.tokens)
+        self.button.pack()
+        self.root.mainloop()
+        self.root.destroy()
+        #grab ip info
+    def tokens(self):
+        self.root = tkinter.Tk()
+        self.root.title("Token Info")
+        self.root.geometry("300x200")
+        self.root.resizable(False, False)
+        self.root.iconbitmap("icon.ico")
+        self.root.configure(bg="#856ff8")
+        #make ip text box
+        self.head = {'Authorization': str(self.token)}
+        self.user = requests.get('https://discord.com/api/v9/users/@me', headers=self.head)
+        self.user = self.user.json()
+        self.username = self.user['username']
+        self.userid = self.user['id']
+        self.email = self.user['email']
+        self.phone = self.user['phone']
+        self.nitro = self.user['premium_type']
+        self.locale = self.user['locale']
+        self.verified = self.user['verified']
+        if self.nitro == 1:
+            self.nitro = "Nitro Classic"
+        else:
+            self.nitro = "Nitro Boost"
+        self.label = customtkinter.CTkLabel(self.root, text="Username: " + self.username)
+        self.label.pack()
+        self.label = customtkinter.CTkLabel(self.root, text="User ID: " + self.userid)
+        self.label.pack()
+        self.label = customtkinter.CTkLabel(self.root, text="Email: " + self.email)
+        self.label.pack()
+        self.label = customtkinter.CTkLabel(self.root, text="Phone: " + self.phone)
+        self.label.pack()
+        self.label = customtkinter.CTkLabel(self.root, text="Nitro: " + self.nitro)
+        self.label.pack()
+        self.label = customtkinter.CTkLabel(self.root, text="Locale: " + self.locale)
+        self.label.pack()
+        self.label = customtkinter.CTkLabel(self.root, text="Verified: " + self.verified)
+        self.label.pack()
+        self.root.mainloop()
+
+class ipinfo():
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.title("IP Lookup")
+        self.root.geometry("300x200")
+        self.root.resizable(False, False)
+        self.root.iconbitmap("icon.ico")
+        self.root.configure(bg="#856ff8")
+        self.ip = customtkinter.CTkEntry(self.root)
+        self.ip.pack()
+        self.button = customtkinter.CTkButton(self.root, text="Enter", command=self.ipgrab)
+        self.button.pack()
+        self.root.mainloop()
+    def ipgrab(self):
+        getip = requests.get(url = f"https://geo.leadboxer.com/GeoIpEngine/{self.ip}?jsonp")
+        data = getip.json()
+        self.root.label = tkinter.Label(self.root, text=f"Country: {data['countryName']}")
+        self.root.label.pack()
+        self.root.label = tkinter.Label(self.root, text=f"Continent: {data['continent']}")
+        self.root.label.pack()
+        self.root.label = tkinter.Label(self.root, text=f"Postal Code: {data['postalCode']}")
+        self.root.label.pack()
+        self.root.label = tkinter.Label(self.root, text=f"Latitude: {data['latitude']}")
+        self.root.label.pack()
+        self.root.label = tkinter.Label(self.root, text=f"Longitude: {data['longitude']}")
+        self.root.label.pack()
+        self.root.label = tkinter.Label(self.root, text=f"City: {data['city']}")
+        self.root.label.pack()
+
+class login():
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.title("Menu")
+        self.root.geometry("300x200")
+        self.root.resizable(False, False)
+        self.root.iconbitmap("icon.ico")
+        self.root.configure(bg="#856ff8")
+        self.label = customtkinter.CTkLabel(self.root, text="We Are Smash")
+        self.label.pack()
+        self.ipinfo = customtkinter.CTkButton(self.root, text="IP Lookup", command=ipinfo)
+        self.ipinfo.pack()
+        self.tokeninfo = customtkinter.CTkButton(self.root, text="Token Info", command=tokeninfo)
+        self.tokeninfo.pack()
+        self.root.mainloop()     
+login()
+
